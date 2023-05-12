358b06f7a1f98330e9d589e938f6258e526981b6
AIproducer <intelligence.producer@gmail.com>
2023-05-12 16:44:17 +0500

    'update'

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
